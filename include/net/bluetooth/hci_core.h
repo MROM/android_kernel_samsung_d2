0332a90a6d6b00e0a50394569b063afe70b463e8
mmontuori <michael.montuori@gmail.com>
2013-07-24 13:05:49 -0400

    invalid prototype

--- include/net/bluetooth/hci_core.h
+++ include/net/bluetooth/hci_core.h
@@ -1037,7 +1037,6 @@ int mgmt_index_added(u16 index);
 int mgmt_index_removed(u16 index);
 int mgmt_set_powered_failed(u16 index, int err);
 int mgmt_powered(u16 index, u8 powered);
-int mgmt_set_powered_failed(struct hci_dev *hdev, int err);
 int mgmt_discoverable(u16 index, u8 discoverable);
 int mgmt_connectable(u16 index, u8 connectable);
 int mgmt_new_key(u16 index, struct link_key *key, u8 bonded);
